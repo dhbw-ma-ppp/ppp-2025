c753e2568933bec8e41df05d6530d536aa8f94e8
DuenenGitH <b.heinen123@icloud.com>
2025-10-22 09:43:07 +0200

    Corrected the wrong type notation of the return value of the simple virtual machine.

--- Exercises/BjoernHeinen01/exercises_02.py
+++ Exercises/BjoernHeinen01/exercises_02.py
@@ -29,7 +29,7 @@
 # print out which value is returned by your function for the following list:
 commands = [1, 12, 2, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 9, 19, 1, 5, 19, 23, 1, 6, 23, 27, 1, 27, 10, 31, 1, 31, 5, 35, 2, 10, 35, 39, 1, 9, 39, 43, 1, 43, 5, 47, 1, 47, 6, 51, 2, 51, 6, 55, 1, 13, 55, 59, 2, 6, 59, 63, 1, 63, 5, 67, 2, 10, 67, 71, 1, 9, 71, 75, 1, 75, 13, 79, 1, 10, 79, 83, 2, 83, 13, 87, 1, 87, 6, 91, 1, 5, 91, 95, 2, 95, 9, 99, 1, 5, 99, 103, 1, 103, 6, 107, 2, 107, 13, 111, 1, 111, 10, 115, 2, 10, 115, 119, 1, 9, 119, 123, 1, 123, 9, 127, 1, 13, 127, 131, 2, 10, 131, 135, 1, 135, 5, 139, 1, 2, 139, 143, 1, 143, 5, 0, 99, 2, 0, 14, 0]
 
-def simple_virtual_machine(commands_list:list[int]) -> int:
+def simple_virtual_machine(commands_list:list[int]):
     """
     This simple vitrual machine just takes a command list
     and does operation as described in the task
@@ -39,9 +39,13 @@ def simple_virtual_machine(commands_list:list[int]) -> int:
     # The function_pointer is the index 
     # to the current instruction value in the command list
     function_pointer:int = 0
+    # I use a var to store the length of the list
+    # so that the while loop doesn't have to call
+    # every cicle the len function
+    length_of_list:int = len(commands_list)
 
     # runs as long there is no break command
-    while (commands_list[function_pointer] != 99):
+    while (function_pointer < length_of_list):
         # Get the 2 values to operate on
         index_of_value_1:int    = commands_list[function_pointer+1]
         index_of_value_2:int   = commands_list[function_pointer+2]
@@ -60,6 +64,9 @@ def simple_virtual_machine(commands_list:list[int]) -> int:
             # multiply numbers:
             case 2:
                 commands_list[indexToWrite] = value_1 * value_2
+            # end programm
+            case 99:
+                return commands_list[0]
             # invalid command:
             case _:
                 print(f"Error! opcode: {commands_list[function_pointer]} is not valid!")
@@ -67,9 +74,9 @@ def simple_virtual_machine(commands_list:list[int]) -> int:
             
         # finally increment the function pointer by 4
         function_pointer += 4
-    
-    # return the result of the virtual machine
-    return commands_list[0]
+
+    # Return None if the machine failed
+    return None
 
 def virtual_machine(data:list[int]) -> int:
     """
