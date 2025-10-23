83a1ff401ee87a4a74a8ff7a3599caca26ffbde4
Jan-Schnitzler03 <janschnitzler03@web.de>
2025-10-23 15:05:54 +0200

    Finale Version zur Aufgabe 2

--- Exercises/Jan_Aufgabe2/exercises_02.py
+++ Exercises/Jan_Aufgabe2/exercises_02.py
@@ -42,7 +42,7 @@ def maschine_funktion(speicher):
 
             speicher[pos3] = speicher[pos1] * speicher[pos2]
         else:
-            raise ValueError("Invalid opcode encountered: {}".format(opcode))
+            raise ValueError("Ungültiger Opcode: {}".format(opcode))
         
         position += 4
 
@@ -53,13 +53,13 @@ def maschine_funktion(speicher):
 # [1, 1, 1, 4, 99, 5, 6, 0, 99] should become [30, 1, 1, 4, 2, 5, 6, 0, 99]
 # Your function should return 30.
 testcase = [1, 1, 1, 4, 99, 5, 6, 0, 99]
-print(maschine_funktion(testcase))  
+print("Ergebnis Test: ",maschine_funktion(testcase))  
 
 
 # print out which value is returned by your function for the following list:
 commands = [1, 12, 2, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 9, 19, 1, 5, 19, 23, 1, 6, 23, 27, 1, 27, 10, 31, 1, 31, 5, 35, 2, 10, 35, 39, 1, 9, 39, 43, 1, 43, 5, 47, 1, 47, 6, 51, 2, 51, 6, 55, 1, 13, 55, 59, 2, 6, 59, 63, 1, 63, 5, 67, 2, 10, 67, 71, 1, 9, 71, 75, 1, 75, 13, 79, 1, 10, 79, 83, 2, 83, 13, 87, 1, 87, 6, 91, 1, 5, 91, 95, 2, 95, 9, 99, 1, 5, 99, 103, 1, 103, 6, 107, 2, 107, 13, 111, 1, 111, 10, 115, 2, 10, 115, 119, 1, 9, 119, 123, 1, 123, 9, 127, 1, 13, 127, 131, 2, 10, 131, 135, 1, 135, 5, 139, 1, 2, 139, 143, 1, 143, 5, 0, 99, 2, 0, 14, 0]
 
-print(maschine_funktion(commands))
+print("Ergebnis: ", maschine_funktion(commands))
 
 ###########################################
 # Write a function that takes an arbitrary number of unnamed arguments
