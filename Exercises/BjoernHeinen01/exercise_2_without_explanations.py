0f12394aa07ef180ed45f3ef5473e6e211f2a4bc
DuenenGitH <b.heinen123@icloud.com>
2025-10-23 18:15:08 +0200

    Created a file with no extra explations. Additionaly, the function in the second task now ignores strings which are not charrakters and not numbers.

--- Exercises/BjoernHeinen01/exercise_2_without_explanations.py
+++ Exercises/BjoernHeinen01/exercise_2_without_explanations.py
@@ -29,55 +29,6 @@
 # print out which value is returned by your function for the following list:
 commands = [1, 12, 2, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 9, 19, 1, 5, 19, 23, 1, 6, 23, 27, 1, 27, 10, 31, 1, 31, 5, 35, 2, 10, 35, 39, 1, 9, 39, 43, 1, 43, 5, 47, 1, 47, 6, 51, 2, 51, 6, 55, 1, 13, 55, 59, 2, 6, 59, 63, 1, 63, 5, 67, 2, 10, 67, 71, 1, 9, 71, 75, 1, 75, 13, 79, 1, 10, 79, 83, 2, 83, 13, 87, 1, 87, 6, 91, 1, 5, 91, 95, 2, 95, 9, 99, 1, 5, 99, 103, 1, 103, 6, 107, 2, 107, 13, 111, 1, 111, 10, 115, 2, 10, 115, 119, 1, 9, 119, 123, 1, 123, 9, 127, 1, 13, 127, 131, 2, 10, 131, 135, 1, 135, 5, 139, 1, 2, 139, 143, 1, 143, 5, 0, 99, 2, 0, 14, 0]
 
-def simple_virtual_machine(commands_list:list[int]):
-    """
-    This simple virtual machine just takes a command list
-    and does operation as described in the task
-    it uses a idiomatic match case logic
-    to choose which operation should be executed.
-    """
-    # The function_pointer is the index 
-    # to the current instruction value in the command list
-    function_pointer:int = 0
-    # I use a var to store the length of the list
-    # so that the while loop doesn't have to call
-    # every cicle the len function
-    length_of_commmand_list:int = len(commands_list)
-
-    # runs as long there is no break command
-    while (function_pointer < length_of_commmand_list):
-        # Get the 2 values to operate on
-        index_of_value_1:int    = commands_list[function_pointer+1]
-        index_of_value_2:int   = commands_list[function_pointer+2]
-
-        # get actual values
-        value_1 = commands_list[index_of_value_1]
-        value_2 = commands_list[index_of_value_2]
-        
-        # get the index for the mashine instruction
-        indexToWrite:int            = commands_list[function_pointer+3]
-
-        match commands_list[function_pointer]:
-            # add numbers:
-            case 1:
-                commands_list[indexToWrite] = value_1 + value_2
-            # multiply numbers:
-            case 2:
-                commands_list[indexToWrite] = value_1 * value_2
-            # end programm
-            case 99:
-                return commands_list[0]
-            # invalid command:
-            case _:
-                print(f"Error! opcode: {commands_list[function_pointer]} is not valid!")
-                return None
-            
-        # increment the function pointer by 4 after the exectution of the command
-        function_pointer += 4
-
-    # Return None if the machine failed
-    return None
-
 def virtual_machine(data:list[int]) -> int:
     """
     The virtual machine takes a list of instruction and data
@@ -112,32 +63,17 @@ def virtual_machine(data:list[int]) -> int:
     """
     func_ptr:int = 0
     
-    
-    # This list stores the instructions
-    # its access time complexity is O(1) while n are the number of instructions
-    # index 0 -> addition
-    # index 1 -> multiplication
     function_field:list = [lambda a, b: a + b,
                            lambda a, b: a * b]
     
-    # if an invalid operation occures 
-    # the except block will get an 
-    # IndexError (IndexErrors occure if you try to 
-    # access a value which is not in the range of a list)
     try:
         while (data[func_ptr] != 99):
-            # Get values to compute
             v1:int = data[data[func_ptr+1]]
             v2:int = data[data[func_ptr+2]]
 
-            # The function index is the task value -1
-            # For example:
-            # if the task value is 1 (addition) the function index is 0 
             func_index:int = data[func_ptr]-1
-            # negative function indexes are not allowed!
-            if func_index < 0: raise IndexError
+            if func_index < 0: raise IndexError()
 
-            # select function, execute function with the values v1 and v2 and store the value
             data[data[func_ptr + 3]] = function_field[func_index](v1, v2)
 
             func_ptr += 4
@@ -149,12 +85,6 @@ def virtual_machine(data:list[int]) -> int:
             raise IndexError(error_message_start + f"function index: {data[func_ptr]-1}")
         except IndexError:
             raise IndexError(error_message_start + f"In addition to this error, the index to the index of the function pointer is out of range!")
-
-# I use commands.copy() to keep an unchanged list of commands 
-# to be able to use it in the virtual_machine function again.
-result_of_the_simple_virtual_machine:int = simple_virtual_machine(commands.copy())
-print("Task 1: Virtual machine:\n")
-print(f"    The result of the simple virtual machine is {result_of_the_simple_virtual_machine}!")
 
 result_of_the_virtual_machine:int = virtual_machine(commands)
 print(f"    The result of the virtual machine is {result_of_the_virtual_machine}!\n\n")
@@ -196,8 +126,6 @@ def split_number_convertable_strings(*args:str):
     Strings which are convertable into numbers are returned in the first list
     and the other strings are returned in the second list.
     """
-    # the underscore of this inner function emphasizes
-    # that it is a private function
     def _is_string_convertible_to_number(string:str) -> bool:
         """
         The function takes a string and checks 
@@ -210,36 +138,12 @@ def split_number_convertable_strings(*args:str):
         else false.
         """
         try:
-            # you have to use eval to transform strings 
-            # into python code
-            # Examples:
-            # 1. complex("math.inf") -> error
-            #    complex(eval("math.inf")) -> complex(math.inf) -> return true
-            # 2. complex("0b0001") -> error
-            #    complex(eval("0b0001")) -> complex(0b0001) -> return true
-            # Additionally, you have to use the 
-            # complex function instead of the float function
-            # to cover complex numbers to
-            
-            num_value = complex(eval(string))
+            num_value = complex(eval(string)) 
 
-            ## Check if the calculated complex value is a special value which is not a number
-            
-            # check if parts of the number are NaN (Not a Number)
-            # this check is neccessary because python is able
-            # to "calculate" with "not a number" values... 
             number_is_not_nan       = not (math.isnan(num_value.real) or math.isnan(num_value.imag))
             number_is_finite        = math.isfinite(num_value.real) and math.isfinite(num_value.imag)
+
             return number_is_not_nan and number_is_finite
-            
-            # Explanation: Why I don't use a simple operator but a cast:
-            # you can NOT use operators like
-            # eval(string) + 1
-            # because the return value of eval(string)
-            # could be a class with an overloaded operator!
-            # Thats why we have to use casts instead of operators
-            # an example could be a vector or lists which adds numbers 
-            # if "vector + number" is called
         except:
             return False
     
@@ -323,7 +227,6 @@ def ComplexNumberToNiceString(num:complex) -> str:
     canceling unnecessary zeros.
     """
     try:
-        # check if the imagenary part of the number can be ignored
         if num.imag == 0:
             num:float = value.real
             if float.is_integer(num):
