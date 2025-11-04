4f0549cf0012e051d14a4906485e074bf4020dea
Y-Gosse <yannickgosse04@gmail.com>
2025-11-04 16:04:33 +0100

    Fixed Imports, removed test0 & test1

--- Exercises/YannickGosse/exercises_02.py
+++ Exercises/YannickGosse/exercises_02.py
@@ -1,3 +1,6 @@
+import sys
+import re
+
 # Write a function that takes as input a list of integers and returns a single integer number.
 # the numbers passed as argument form the working memory of a simulated computer.
 # this computer will start by looking at the first value in the list passed to the function.
@@ -29,11 +32,6 @@
 # print out which value is returned by your function for the following list:
 commands = [1, 12, 2, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 9, 19, 1, 5, 19, 23, 1, 6, 23, 27, 1, 27, 10, 31, 1, 31, 5, 35, 2, 10, 35, 39, 1, 9, 39, 43, 1, 43, 5, 47, 1, 47, 6, 51, 2, 51, 6, 55, 1, 13, 55, 59, 2, 6, 59, 63, 1, 63, 5, 67, 2, 10, 67, 71, 1, 9, 71, 75, 1, 75, 13, 79, 1, 10, 79, 83, 2, 83, 13, 87, 1, 87, 6, 91, 1, 5, 91, 95, 2, 95, 9, 99, 1, 5, 99, 103, 1, 103, 6, 107, 2, 107, 13, 111, 1, 111, 10, 115, 2, 10, 115, 119, 1, 9, 119, 123, 1, 123, 9, 127, 1, 13, 127, 131, 2, 10, 131, 135, 1, 135, 5, 139, 1, 2, 139, 143, 1, 143, 5, 0, 99, 2, 0, 14, 0]
 
-
-test0 = [1, 0, 0, 0, 99]
-test1 = [1, 1, 1, 4, 99, 5, 6, 0, 99]
-
-import sys
 
 def task1(input_list):
     i = 0
@@ -67,8 +65,6 @@ print("output task1: ", task1(commands), "\n")
 # Think of some good inputs to test this functionality, write down at least three
 # examples and verify that the output for these examples is correct.
 
-
-import re
 
 test_list0 = ["6", "8.4", "II", "A", "hi", "!", "I", "V", "0o456", "0x678a", "Wort", "r"]
 test_list1 = ["12","0O4136", "3.14", "IV", "ix", "B", "z", "@", "#", "hello", "abc", "83176cda"]
