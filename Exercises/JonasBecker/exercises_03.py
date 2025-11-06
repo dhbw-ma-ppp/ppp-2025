2a858a48bd04c9a16c973293ecbe206c3d997c19
Jonas Becker <AquaJo@users.noreply.github.com>
2025-11-06 16:05:01 +0100

    feat: part 2 and some code style changes

--- Exercises/JonasBecker/exercises_03.py
+++ Exercises/JonasBecker/exercises_03.py
@@ -12,6 +12,8 @@ from __future__ import (
 #  get_balance() → returns the balance
 #  transfer(other_account, amount) -> It should withdraw from one account and deposit into the other.
 #  Try depositing, withdrawing and transferring amounts and print the balance each time.
+
+
 class BankAccount:
     def __init__(self):
         self._balance: float = 0
@@ -43,11 +45,14 @@ class BankAccount:
 
 
 # Testing
+print("1)")
+
 account1 = BankAccount()
 account2 = BankAccount()
-
-print(f"Initial account 1 balance: {account1.balance}")
-print(f"Initial account 2 balance: {account2.balance}")
+print(
+    f"Initial account 1 balance: {account1.balance}\n"
+    f"Initial account 2 balance: {account2.balance}"
+)
 
 account1.deposit(100)
 print(f"After depositing 100 into Account 1: {account1.balance}")
@@ -58,9 +63,11 @@ else:
     print("Withdrawal failed")
 
 if account1.transfer(account2, 50):
-    print(f"After transferring 50 from Account 1 to Account 2:")
-    print(f"Account 1 balance: {account1.balance}")
-    print(f"Account 2 balance: {account2.balance}")
+    print(
+        "After transferring 50 from Account 1 to Account 2:\n"
+        f"Account 1 balance: {account1.balance}\n"
+        f"Account 2 balance: {account2.balance}"
+    )
 else:
     print("Transfer failed")
 
@@ -75,6 +82,45 @@ if not account1.withdraw(100):
 #   I should be able to iterate over all cards in the deck.
 #   Printing a cards string representation should give me a nice, readable description of that card.
 
+
+class Card:
+    def __init__(self, rank: str, suit: str):
+        self.rank = rank
+        self.suit = suit
+
+    def __repr__(self) -> str:
+        return f"{self.rank} of {self.suit}"
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = ["Diamonds", "Hearts", "Spades", "Clubs"]
+
+    def __init__(self):
+        self._cards: list[Card] = [
+            Card(rank, suit) for suit in self.suits for rank in self.ranks
+        ]
+
+    def __len__(self) -> int:
+        return len(self._cards)
+
+    def __getitem__(self, position: int) -> Card:
+        return self._cards[position]
+
+    def __iter__(self):
+        return iter(self.cards) # not needed here but recommended
+
+
+print("2)")
+
+french_deck = FrenchDeck()
+print(
+    f"The French Deck has {len(french_deck)} cards.\n"
+    f"First Card: {french_deck[0]}, Last Card: {french_deck[-1]}"
+)
+for card in french_deck:
+    pass
+print("The french_deck is iterable!")
 
 # PART 3:
 # Create a second class that represents a deck of cards usable for Skat -- it should only contain cards from 7 upwards.
