9845090e8ba49f03a3da58a344eb51c876cd8329
Bene <Benevolker@gmail.com>
2025-11-11 19:15:48 +0100

    Habe die angemerkten Dinge nochmal teilweise überarbeitet. Die Deck-Klasse und den negativen Betrag beim Abheben nochmal geändert. Danke fürs gute Review!

--- Exercises/benedict_volker/exercises_03_Bene.py
+++ Exercises/benedict_volker/exercises_03_Bene.py
@@ -11,21 +11,29 @@ class BankAccount:
     def __init__(self):
         self._account_balance = 0
     def deposit(self, amount):
-        self._account_balance += amount
+        if amount > 0:
+            self._account_balance += amount
+        else:
+            print("The inserted amount was negative")
+            raise ValueError
     def withdraw(self, amount):
-        if self._account_balance >= amount:
+        if self._account_balance >= amount and amount > 0:
             self._account_balance -= amount
         else:
-            print(f"you do not have enough balance: {self._account_balance}")
+            print(f"you do not have enough balance: {self._account_balance} Euro, or the requested amount was negative")
             raise ValueError
     def get_balance(self):
         return self._account_balance
     def transfer(self, other_account, amount):
-        try: 
-            self.withdraw(amount)
-        except ValueError:
-            print("transfer failed")
-            return 
+        if amount > 0:
+            try: 
+                self.withdraw(amount)
+            except ValueError:
+                print("transfer failed")
+                return 
+        else:
+            print("The inserted amount was negative")
+            raise ValueError
         other_account.deposit(amount)
 
 #Tests
@@ -47,21 +55,30 @@ print(acc1.get_balance(), acc2.get_balance())
 #   I should be able to iterate over all cards in the deck.
 #   Printing a cards string representation should give me a nice, readable description of that card.
 
-class CardDeck:
+class Deck:
     def __init__(self):
-        self.cards = ["2 \u2666","3 \u2666","4 \u2666","5 \u2666","6 \u2666","7 \u2666","8 \u2666","9 \u2666","10\u2666","B \u2666","D \u2666","K \u2666","A \u2666",
-                      "2 \u2665","3 \u2665","4 \u2665","5 \u2665","6 \u2665","7 \u2665","8 \u2665","9 \u2665","10\u2665","B \u2665","D \u2665","K \u2665","A \u2665",
-                      "2 \u2660","3 \u2660","4 \u2660","5 \u2660","6 \u2660","7 \u2660","8 \u2660","9 \u2660","10\u2660","B \u2660","D \u2660","K \u2660","A \u2660",
-                      "2 \u2663","3 \u2663","4 \u2663","5 \u2663","6 \u2663","7 \u2663","8 \u2663","9 \u2663","10\u2663","B \u2663","D \u2663","K \u2663","A \u2663"]
-    
-    def pick_card(self, cardindex):
+        self.cards = self.CARDS
+
+    def __getitem__(self, cardindex: int):
         if cardindex > len(self.cards) or cardindex < 0:
             print("Index nicht im Kartendeck enthalten. Er muss zwischen 0 und 51 liegen")
             return 
         
         return self.cards[cardindex]
+    
+    def __len__(self) -> int:
+        return len(self.cards)
+    
     def give_whole_deck(self):
-            return self.cards
+        return self.cards
+       
+
+class CardDeck(Deck):
+    CARDS = ["2 \u2666","3 \u2666","4 \u2666","5 \u2666","6 \u2666","7 \u2666","8 \u2666","9 \u2666","10\u2666","B \u2666","D \u2666","K \u2666","A \u2666",
+                      "2 \u2665","3 \u2665","4 \u2665","5 \u2665","6 \u2665","7 \u2665","8 \u2665","9 \u2665","10\u2665","B \u2665","D \u2665","K \u2665","A \u2665",
+                      "2 \u2660","3 \u2660","4 \u2660","5 \u2660","6 \u2660","7 \u2660","8 \u2660","9 \u2660","10\u2660","B \u2660","D \u2660","K \u2660","A \u2660",
+                      "2 \u2663","3 \u2663","4 \u2663","5 \u2663","6 \u2663","7 \u2663","8 \u2663","9 \u2663","10\u2663","B \u2663","D \u2663","K \u2663","A \u2663"]
+    
 
 
 
@@ -69,35 +86,25 @@ class CardDeck:
 # Create a second class that represents a deck of cards usable for Skat -- it should only contain cards from 7 upwards.
 # It should offer all the same functionality of the first class.
 
-class SkatDeck:
-    def __init__(self):
-        self.cards = ["7 \u2666","8 \u2666","9 \u2666","10\u2666","B \u2666","D \u2666","K \u2666","A \u2666",
-                      "7 \u2665","8 \u2665","9 \u2665","10\u2665","B \u2665","D \u2665","K \u2665","A \u2665",
-                      "7 \u2660","8 \u2660","9 \u2660","10\u2660","B \u2660","D \u2660","K \u2660","A \u2660",
-                      "7 \u2663","8 \u2663","9 \u2663","10\u2663","B \u2663","D \u2663","K \u2663","A \u2663"]
-    
-    def pick_card(self, cardindex):
-        if cardindex > len(self.cards) or cardindex < 0:
-            print("Index nicht im Kartendeck enthalten. Er muss zwischen 0 und 51 liegen")
-            return 
-        
-        return self.cards[cardindex]
-    def give_whole_deck(self):
-        return self.cards
+class SkatDeck(Deck):
+    CARDS = ["2 \u2666","3 \u2666","4 \u2666","5 \u2666","6 \u2666","7 \u2666","8 \u2666","9 \u2666","10\u2666","B \u2666","D \u2666","K \u2666","A \u2666",
+                      "2 \u2665","3 \u2665","4 \u2665","5 \u2665","6 \u2665","7 \u2665","8 \u2665","9 \u2665","10\u2665","B \u2665","D \u2665","K \u2665","A \u2665",
+                      "2 \u2660","3 \u2660","4 \u2660","5 \u2660","6 \u2660","7 \u2660","8 \u2660","9 \u2660","10\u2660","B \u2660","D \u2660","K \u2660","A \u2660",
+                      "2 \u2663","3 \u2663","4 \u2663","5 \u2663","6 \u2663","7 \u2663","8 \u2663","9 \u2663","10\u2663","B \u2663","D \u2663","K \u2663","A \u2663"]
 
 # Write some code to test the functionality of both kinds of decks. (You can use `assert` to make sure your classes behave the way you expect them to.)
 
 deck1 = CardDeck()
-print(f"You have picked the {deck1.pick_card(4)}")
+print(f"You have picked the {deck1[4]}")
 print(f"Das Deck besteht aus: {deck1.give_whole_deck()}")
 
 skatdeck1 = SkatDeck()
-print(f"You have picked the {skatdeck1.pick_card(15)}")
+print(f"You have picked the {skatdeck1[0]}")
 print(f"Das Deck besteht aus: {skatdeck1.give_whole_deck()}")
 
 res = "6 \u2666"
-assert res == deck1.pick_card(4)
-assert "7 \u2666" == skatdeck1.pick_card(0)
+assert res == deck1[4]
+assert "2 \u2666" == skatdeck1[0]
 
 # PART 4:
 # write a function that accepts two numbers, a lower bound and an upper bound.
