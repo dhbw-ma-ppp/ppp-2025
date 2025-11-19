fc1517809e2743c286ae49e11bc0bd6b550082fa
HannaFiedler <hannafiedler@gmx.net>
2025-11-19 18:04:52 +0100

    Hanna 04
    * Update exercises_04.py
    
    * Update exercises_04.py

--- Exercises/exercises_04.py
+++ Exercises/exercises_04.py
@@ -52,3 +52,91 @@
 # Finally, run you code for the following instructions; when asked for input provide the number '5'. The program should print a single number when executed.
 # Please take note of that number in your PR, so I don't need to run all the files myself :)
 commands = [3,225,1,225,6,6,1100,1,238,225,104,0,1101,40,71,224,1001,224,-111,224,4,224,1002,223,8,223,101,7,224,224,1,224,223,223,1102,66,6,225,1102,22,54,225,1,65,35,224,1001,224,-86,224,4,224,102,8,223,223,101,6,224,224,1,224,223,223,1102,20,80,225,101,92,148,224,101,-162,224,224,4,224,1002,223,8,223,101,5,224,224,1,224,223,223,1102,63,60,225,1101,32,48,225,2,173,95,224,1001,224,-448,224,4,224,102,8,223,223,1001,224,4,224,1,224,223,223,1001,91,16,224,101,-79,224,224,4,224,1002,223,8,223,101,3,224,224,1,224,223,223,1101,13,29,225,1101,71,70,225,1002,39,56,224,1001,224,-1232,224,4,224,102,8,223,223,101,4,224,224,1,223,224,223,1101,14,59,225,102,38,143,224,1001,224,-494,224,4,224,102,8,223,223,101,3,224,224,1,224,223,223,1102,30,28,224,1001,224,-840,224,4,224,1002,223,8,223,101,4,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,107,677,226,224,1002,223,2,223,1005,224,329,1001,223,1,223,8,226,226,224,102,2,223,223,1006,224,344,101,1,223,223,7,226,677,224,1002,223,2,223,1005,224,359,101,1,223,223,1007,677,226,224,1002,223,2,223,1005,224,374,1001,223,1,223,1007,677,677,224,1002,223,2,223,1006,224,389,101,1,223,223,1008,226,226,224,1002,223,2,223,1005,224,404,1001,223,1,223,108,677,226,224,1002,223,2,223,1006,224,419,1001,223,1,223,1108,677,226,224,102,2,223,223,1006,224,434,1001,223,1,223,108,226,226,224,1002,223,2,223,1005,224,449,101,1,223,223,7,677,677,224,1002,223,2,223,1006,224,464,1001,223,1,223,8,226,677,224,1002,223,2,223,1005,224,479,1001,223,1,223,107,226,226,224,102,2,223,223,1006,224,494,101,1,223,223,1007,226,226,224,1002,223,2,223,1005,224,509,1001,223,1,223,1107,226,677,224,102,2,223,223,1005,224,524,1001,223,1,223,108,677,677,224,1002,223,2,223,1005,224,539,101,1,223,223,1107,677,226,224,102,2,223,223,1005,224,554,1001,223,1,223,107,677,677,224,1002,223,2,223,1005,224,569,101,1,223,223,8,677,226,224,102,2,223,223,1005,224,584,1001,223,1,223,7,677,226,224,102,2,223,223,1006,224,599,101,1,223,223,1008,677,677,224,1002,223,2,223,1005,224,614,101,1,223,223,1008,677,226,224,102,2,223,223,1006,224,629,1001,223,1,223,1108,677,677,224,102,2,223,223,1006,224,644,101,1,223,223,1108,226,677,224,1002,223,2,223,1005,224,659,1001,223,1,223,1107,226,226,224,102,2,223,223,1006,224,674,1001,223,1,223,4,223,99,226]
+
+def simulate_computer(liste):
+    pos = 0 #start at position 0
+    while True:
+        opcode = liste[pos] % 100               #last 2 digits = opcode
+        mode1 = (liste[pos] // 100) % 10        #3rd last digit = mode of parameter 1
+        mode2 = (liste[pos] // 1000) % 10       #4th last digit = mode of parameter 2
+
+        match opcode:
+            case 1: #addition
+                parameter1 = get_parameter(liste, pos+1, mode1)
+                parameter2 = get_parameter(liste, pos+2, mode2)
+                target = liste[pos+3]           #target is always in position mode
+                liste[target] = parameter1 + parameter2
+                pos+=4
+
+            case 2: #multiplication
+                parameter1 = get_parameter(liste, pos+1, mode1)
+                parameter2 = get_parameter(liste, pos+2, mode2)
+                target = liste[pos+3]
+                liste[target] = parameter1*parameter2
+                pos+=4
+
+            case 3: #input
+                target = liste[pos+1]
+                liste[target] = int(input("Please enter a number:"))
+                pos += 2
+
+            case 4: #output
+                parameter1 = get_parameter(liste, pos+1, mode1)
+                output = parameter1
+                pos += 2
+
+            case 5: #jump-if-true
+                parameter1 = get_parameter(liste, pos+1, mode1)
+                parameter2 = get_parameter(liste, pos+2, mode2)
+                if parameter1 != 0:
+                    pos = parameter2
+                else:
+                    pos += 3
+
+            case 6: #jump-if-false
+                parameter1 = get_parameter(liste, pos+1, mode1)
+                parameter2 = get_parameter(liste, pos+2, mode2)
+                if parameter1 == 0:
+                    pos = parameter2
+                else:
+                    pos += 3
+
+            case 7: #less than
+                parameter1 = get_parameter(liste, pos+1, mode1)
+                parameter2 = get_parameter(liste, pos+2, mode2)
+                target = liste[pos+3]
+                if parameter1 < parameter2:
+                    liste[target] = 1
+                else:
+                    liste[target] = 0
+                pos += 4
+
+            case 8: #equals
+                parameter1 = get_parameter(liste, pos+1, mode1)
+                parameter2 = get_parameter(liste, pos+2, mode2)
+                target = liste[pos+3]
+                if parameter1 == parameter2:
+                    liste[target] = 1
+                else:
+                    liste[target] = 0
+                pos += 4
+
+            case 99:
+                return output
+            
+            case _:
+                print("Something went wrong.")
+                return None
+            
+def get_parameter(liste, pos, mode):    #returns number depending on mode
+    if mode == 0:
+        return liste[liste[pos]]        #position mode
+    elif mode == 1:
+        return liste[pos]               #immediate mode
+
+test1 = [3,9,8,9,10,9,4,9,99,-1,8]
+test2 = [3,3,1107,-1,8,3,4,3,99]
+test3 = [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]
+
+print("Output:", simulate_computer(commands))
+#Output: 16694270
