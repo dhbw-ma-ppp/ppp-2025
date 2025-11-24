432bb684a1262a831775d79057dbd2c0762d3b10
Jonas Becker <AquaJo@users.noreply.github.com>
2025-11-24 23:30:14 +0100

    feat: improve by e.g. using enums and imports

--- Exercises/JonasBecker/exercises_05.py
+++ Exercises/JonasBecker/exercises_05.py
@@ -1,49 +1,147 @@
-import time
+from enum import Enum
 
 import numpy as np
 from numpy import ndarray
 
 import matplotlib.pyplot as plt
-from matplotlib.backend_bases import KeyEvent
 from matplotlib.colors import ListedColormap
 
+from utils.user import get_user_input
 from utils.computer import calculate_number_through_memory_list
+from utils.mathplotlib.user import UserController
+
+# ---------- Enums ----------
+
+
+class Direction(Enum):
+    LEFT = -1
+    STAY = 0
+    RIGHT = 1
+
+
+class PlayMode(Enum):
+    BOT = 0
+    LIVE = 1
+    RELAXED = 2
+
+
+class Tile(Enum):
+    EMPTY = (0, "#2E294E")
+    WALL = (1, "#D90368")
+    BLOCK = (2, "#F1E9DA")
+    PADDLE = (3, "#FF69B4")
+    BALL = (4, "#FFD400")
+
+    def __new__(
+        cls, value, color
+    ):  # note: runs before init, creates object (special in enum)
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.color = color
+        return obj
+
+
+# ---------- Global Constants ----------
+
+DIRECTIONS_TO_KEY = {
+    Direction.LEFT: ["a", "left"],
+    Direction.STAY: ["s", "down"],
+    Direction.RIGHT: ["d", "right"],
+}
+
+COLORMAP = [tile.color for tile in Tile]
+
+
+# ---------- Global State ----------
+play_mode_number = get_user_input(  # ask for mode as CLI before showing plot
+    "Please enter playmode:\n0 -> Bot,\n1 -> Ball is moving by itself,\n2 -> Ball is moving only on Input\nAnswer: ",
+    type=int,
+    accept=[0, 1, 2],
+)
+play_mode = PlayMode(play_mode_number)
 
 plt.ion()
 fig, ax = plt.subplots()
+user_controller = UserController(fig)
 
-# ---------- Input Handling ----------
-user_inputs: list[int] = []
-
-
-def on_key(event: KeyEvent):
-    event_name = event.key
-    if not isinstance(event_name, str): # or is None
-        return
-    lower_event_name: str = event_name.lower()
-    if lower_event_name in ["a", "left"]:
-        user_inputs.append(-1)
-    elif lower_event_name in ["d", "right"]:
-        user_inputs.append(1)
-    elif lower_event_name in ["s", "down"]:
-        user_inputs.append(0)
+create_grid_base: bool = True
+grid: ndarray
+score: int = 0
+ball_x: int = -1
+paddle_x: int = -1
 
 
-def get_user_input(waiting: bool = False, timeout: float = 0.75) -> int:
-    start_time = time.time()
-    while True:
-        if user_inputs:
-            return user_inputs.pop(0)
-        plt.pause(0.05)
-        if time.time() - start_time >= timeout and not waiting:
-            return 0
+# ---------- Functions ----------
 
 
-fig.canvas.mpl_connect("key_press_event", on_key)
+def draw():
+    ax.clear()
+    fig.canvas.manager.set_window_title("Breakout Game")
+    ax.set_title(f"Score: {score}", color="#F1E9DA")
+    fig.patch.set_facecolor("#2E294E")
+    cmap = ListedColormap(COLORMAP)
+    ax.imshow(grid, cmap=cmap, origin="upper")
+    ax.axis("off")
+    plt.tight_layout(pad=0)
+    plt.draw()
+
+
+def get_next_move() -> int:
+    timeout: int | None = None
+
+    if play_mode == PlayMode.BOT:
+        plt.pause(0.001)
+        x_distance_to_ball = ball_x - paddle_x
+        if x_distance_to_ball > 0:
+            return Direction.RIGHT.value
+        if x_distance_to_ball < 0:
+            return Direction.LEFT.value
+        else:
+            return Direction.STAY.value
+    elif play_mode == PlayMode.LIVE:
+        timeout = 1
+
+    next_input_direction = user_controller.get_next_key_input(
+        case_sensitive=False,
+        accept=DIRECTIONS_TO_KEY.values(),
+        replace_by_key=DIRECTIONS_TO_KEY,
+        timeout=timeout,
+    )
+
+    return (next_input_direction or Direction.STAY).value
+
+
+def on_delta_stacked_output(val: list[int]):
+    global create_grid_base, grid, score, ball_x, paddle_x
+
+    triplets: list[tuple] = [tuple(val[i : i + 3]) for i in range(0, len(val), 3)]
+
+    if create_grid_base:
+        max_x = max(t[0] for t in triplets)
+        max_y = max(t[1] for t in triplets)
+        grid = np.zeros((max_y + 1, max_x + 1), dtype=int)
+        create_grid_base = False
+
+    for x, y, tile_number in triplets:
+        if x == -1 and y == 0:
+            score = tile_number
+            continue
+
+        tile = Tile(tile_number)
+        if tile == Tile.PADDLE:
+            paddle_x = x
+        elif tile == Tile.BALL:
+            ball_x = x
+
+        grid[y, x] = tile.value
+
+    draw()
 
 
 # ---------- Load external file ----------
+
 numbers: list[int] = []
+
 with open("breakout_commands.txt", "r") as f:
     for line in f:
         line = line.strip()
@@ -51,53 +149,15 @@ with open("breakout_commands.txt", "r") as f:
             numbers.append(int(line))
 
 
-# ---------- Plotting ----------
-
-
-def draw(grid: ndarray, score):
-    ax.clear()
-    fig.canvas.manager.set_window_title("Breakout Game")
-    ax.set_title(f"Score: {score}", color="#F1E9DA")
-    colors: list[str] = ["#2E294E", "#D90368", "#F1E9DA", "#FF69B4", "#FFD400"]
-    fig.patch.set_facecolor("#2E294E")
-    cmap: ListedColormap = ListedColormap(colors)
-    ax.imshow(grid, cmap=cmap, origin="upper")
-    ax.axis("off")
-    plt.tight_layout(pad=0)
-    plt.draw()
-
-
-def create_grid_update_callback():
-    create_grid_base: bool = True
-    grid: ndarray
-    score: int = 0
-
-    def on_delta_stacked_output(val: list[int]):
-        nonlocal create_grid_base, grid, score
-
-        triplets: list[tuple] = [tuple(val[i : i + 3]) for i in range(0, len(val), 3)]
-        if create_grid_base:
-            max_x: int = max(t[0] for t in triplets)
-            max_y: int = max(t[1] for t in triplets)
-
-            grid = np.zeros((max_y + 1, max_x + 1), dtype=int)
-            create_grid_base = False
-
-        for x, y, tile in triplets:
-            if x == -1 and y == 0:
-                score = tile
-                continue
-            grid[y, x] = tile
-        draw(grid, score)
-
-    return on_delta_stacked_output
-
-
 # ---------- Start ----------
-on_output = create_grid_update_callback()
-numbers[0] = 2
+
+numbers[0] = 2  # set interactive play mode for Int-Code-Program
+
 calculate_number_through_memory_list(
-    numbers, [0], input_callback=get_user_input, output_delta_stacked_callback=on_output
+    numbers,
+    input_callback=get_next_move,
+    output_delta_stacked_callback=on_delta_stacked_output,
 )
+
 plt.ioff()
 plt.show()
--- Exercises/JonasBecker/utils/user.py
+++ Exercises/JonasBecker/utils/user.py
@@ -1,9 +1,17 @@
 def get_user_input(
-    prompt: str = "Enter something: ", type=str
+    prompt: str = "Enter something: ", type=str, accept: list | None = None
 ):  # type is some type, returns some type
     while True:
         try:
-            user_input = input(prompt)
-            return type(user_input)
+            user_input = type(input(prompt))
+
+            if isinstance(accept, list):
+                if user_input in accept:
+                    return user_input
+                else:
+                    print(f"Invalid input! Please only provide a valid choice.")
+                    continue
+
+            return user_input
         except ValueError:
             print(f"Invalid input! Please enter a value of type {type.__name__}.")
