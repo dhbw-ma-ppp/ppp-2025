6d911c04baf8947a39234168580fe9f9044d8ed3
clarakobitschmeyer <clarakobitschmeyer@gmail.com>
2025-11-27 10:52:31 +0100

    Clara uebung05 (#102)
    * clara_uebung05
    
    * clara_ubeung05
    
    * clara_uebung05
    
    * Little changes
    
    Removed unnecessary normalization in imshow and adjusted max_x and max_y calculations.

---
+++ Exercises/clara_uebung05/exercises_05.py
@@ -0,0 +1,246 @@
+# to solve todays exercise you will need a fully functional int-computer
+# a fully functional int-computer has some additional features compared to the 
+# last one you implemented.
+# you can either use my implementation of an int-computer (see AtrejuTauschinsky/reference_int_computer.py)
+# or you can extend your own int-computer with the necessary features. If you want to extend your own
+# the features you need to implement will be described at the bottom.
+#
+#
+# We will run 'breakout' -- the arcade game -- on our simulated computer. 
+# (https://en.wikipedia.org/wiki/Breakout_(video_game))
+# The code for the computer will be provided under data/breakout_commands.txt
+# the code will produce outputs in triplets. every triplet that is output
+# specifies (x-position, y-position, tile_type).
+# tiles can be of the following types:
+# 0: empty tile
+# 1: wall. walls are indestructible
+# 2: block. blocks can be destroyed by the ball
+# 3: paddle. the paddle is indestructible
+# 4: ball. the ball moves diagonally and bounces off objects
+# 
+# EXAMPLE:
+# a sequence of output values like 1, 2, 3, 6, 5, 4 would
+#  - draw a paddle (type 3) at x=1, y=2
+#  - draw the ball (type 4) at x=6, y=5
+#
+#
+# PART 1:
+# run the game until it exits. Analyse the output produced during the run, and create
+# a visual representation of the screen display using one of the plotting libraries we discussed today.
+# mark the different tile types as different colors or symbols. Upload the picture with your PR.
+#
+# PART 2:
+# The game didn't actually run in part 1, it just drew a single static screen.
+# Change the first instruction of the commands from 1 to 2. Now the game will actually run.
+# when the game actually runs you need to provide inputs to steer the paddle. whenever the computer
+# requests you to provide an input, you can chose to provide
+# -  0: the paddle remains in position
+# - -1: move the paddle to the left
+# - +1: move the paddle to the right
+#
+# the game also outputs a score. when an output triplet is in position (-1, 0) the third value of
+# the triplet is not a tile type, but your current score.
+# You need to beat the game by breaking all tiles without ever letting the ball cross the bottom 
+# edge of the screen. What is your high-score at the end of the game? provide the score as part of your PR.
+#
+# BONUS: (no extra points, just for fun)
+# make a movie of playing the game :)
+
+
+import operator
+from matplotlib import pyplot as plt
+import numpy as np
+
+class IntComputer:
+    def __init__(self, input_getter, output_collector):
+        def get_input(modes):
+            target_mode = modes % 10
+            if target_mode == 2:
+                target = self.memory.get(self.ip, 0) + self.relative_mode_offset
+            else:
+                target = self.memory.get(self.ip, 0)
+            self.memory[target] = input_getter()
+            self.ip += 1
+
+        def write_output(modes):
+            x, = self.get_function_arguments(modes, 1)
+            output_collector(x)
+
+        def set_offset(modes):
+            x,  = self.get_function_arguments(modes, 1)
+            self.relative_mode_offset += x
+
+        self.function_map = {
+            1: self._make_register_setter(operator.add),
+            2: self._make_register_setter(operator.mul),
+            3: get_input,
+            4: write_output,
+            5: self._make_ip_setter(operator.ne),
+            6: self._make_ip_setter(operator.eq),
+            7: self._make_register_setter(operator.lt),
+            8: self._make_register_setter(operator.eq),
+            9: set_offset,
+        }
+
+    def _make_register_setter(self, func):
+        # make generic function that sets a register
+        def f(modes):
+            x, y = self.get_function_arguments(modes, 2)
+            target_mode = (modes // (10**2)) % 10
+            if target_mode == 2:
+                target = self.memory.get(self.ip, 0) + self.relative_mode_offset
+            else:
+                target = self.memory.get(self.ip, 0)
+            self.memory[target] = int(func(x, y))
+            self.ip += 1
+        return f
+
+    def _make_ip_setter(self, func, comparison_value=0):
+        # make a generic function that sets the instruction pointer
+        def f(modes):
+            x, y = self.get_function_arguments(modes, 2)
+            if func(x, comparison_value):
+                self.ip = y
+        return f
+
+    def _resolve_argument_value(self, arg_mode, arg_value):
+        if arg_mode == 0:
+            return self.memory.get(arg_value, 0)
+        if arg_mode == 1:
+            return arg_value
+        if arg_mode == 2:
+            return self.memory.get(arg_value + self.relative_mode_offset, 0)
+
+    def get_function_arguments(self, modes, n_args):
+        arg_values = [self.memory[self.ip + x] for x in range(n_args)]
+        arg_modes = [(modes // (10**i)) % 10 for i in range(n_args)]
+        arguments = [self._resolve_argument_value(mode, value) for mode, value in zip(arg_modes, arg_values)]
+        self.ip += n_args
+        return arguments
+
+    def split_command_and_modes(self):
+        command = self.memory[self.ip]
+        self.ip += 1
+        return command % 100, command // 100
+
+    def run(self, data):
+        self.memory = {i: v for i, v in enumerate(data)}
+        self.ip = 0
+        self.relative_mode_offset = 0
+
+        while True:
+            opcode, modes = self.split_command_and_modes()
+            if opcode == 99:
+                break
+
+            # calculate function arguments
+            opcode_function = self.function_map[opcode]
+            opcode_function(modes)
+
+def output_collector(value):
+    output_values.append(value)
+
+output_buffer = []
+game_state = {
+    'score': 0,
+    'paddle_x': 0,
+    'ball_x': 0,
+}
+def output_collector02(value):
+    global output_buffer
+    output_buffer.append(value)
+
+    if len(output_buffer) == 3:
+        x, y, tile_type = output_buffer
+        output_buffer = []
+
+        if x == - 1 and y == 0:
+            game_state['score'] = tile_type
+
+        elif tile_type == 3:
+            game_state['paddle_x'] = x
+        elif tile_type == 4:
+            game_state['ball_x'] = x
+
+def input_getter02():
+    ball_x = game_state['ball_x']
+    paddle_x = game_state['paddle_x']
+
+    if ball_x > paddle_x:
+        return 1
+    elif ball_x < paddle_x:
+        return -1
+    else:
+        return 0
+
+def visualize_game(screen_array):
+    colors = ['white', 'gray', 'violet', 'purple', 'pink']
+    cmap = plt.cm.colors.ListedColormap(colors) #a color map
+
+    plt.figure(figsize = (max_x / 10, max_y / 5))
+    plt.imshow(screen_array, cmap = cmap)
+    
+    plt.gca().invert_xaxis() 
+    plt.gca().set_axis_off()
+    plt.grid(False)
+    plt.tight_layout()
+    plt.show()
+
+with open("C:/Users/X1 Yoga/OneDrive/Dokumente/DHBW/Python/Programme/breakout_commands.txt", 'r') as f:
+    commands = [int(x) for x in f.read().split('\n')]
+
+output_values = []
+
+#Part 1
+
+computer01 = IntComputer(None, output_collector)
+computer01.run(commands)
+
+#Part 2
+commands[0] = 2
+
+computer02 = IntComputer(input_getter02, output_collector02)
+computer02.run(commands)
+
+final_score = game_state['score']
+
+print("The final score is:", final_score) #The final score is: 17159
+
+screen_map = {}
+score = 0
+for i in range(0, len(output_values), 3):
+    x, y, tile_type = output_values[i:i+3]
+    
+    screen_map[(x, y)] = tile_type
+
+max_x = max(k[0] for k in screen_map.keys())
+max_y = max(k[1] for k in screen_map.keys())
+
+screen_array = np.zeros((max_y + 1, max_x + 1), dtype=int)
+
+for (x, y), tile in screen_map.items():
+    screen_array[y, x] = tile
+
+visualize_game(screen_array)
+
+# COMPLETE INT COMPUTER
+# This is only relevant if you decide to extend your own implementation with the necessary features.
+# If you decide to use my implementation you can ignore this part.
+#
+#
+# - The computer needs to implement memory much /larger/ than the set of initial commands.
+#   Any memory address not part of the initial commands can be assumed to be initialized to 0.
+#   (only positive addresses are valid).
+# - You need to support a new parameter mode, 'relative mode', denoted as mode 2 in the 'mode' part
+#   of the instructions.
+#   Relative mode is similar to position mode (the first access mode you implemented). However, 
+#   parameters in relative mode count not from 0, but from a value called 'relative offset'. 
+#   When the computer is initialized, the relative offset is initialized to 0, and as long as it remains
+#   0 relative mode and position mode are identical.
+#   In general though parameters in relative mode address the memory location at 'relative offset + parameter value'.
+#   EXAMPLE: if the relative offset is 50, the mode is 2, and the value you read from memory is 7 you should 
+#     retrieve data from the memory address 57.
+#     Equally, if you read -7, you should retrieve data from the memory address 43.
+#   This applies to both read- and write operations.
+# - You need to implement a new opcode, opcode 9. opcode 9 adjusts the relative offset by the value of its only parameter.
+#   the offset increases by the value of the parameter (or decreases if that value is negative).
