025f1cc052db172decfcc84da98a792482156c97
DuenenGitH <b.heinen123@icloud.com>
2025-12-05 14:24:51 +0100

    UPdate!

--- Exercises/Wie viel Ihr Auto Wert ist/Projekt_Woche8/data/DecissionTree.py
+++ Exercises/Wie viel Ihr Auto Wert ist/Projekt_Woche8/data/DecissionTree.py
@@ -1,5 +1,56 @@
-from data import df as car_price_data
+import pandas as pd
 
-print(car_price_data.head())
-print(car_price_data.info())
+from data import df
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 
+
+if __name__ == "__main__":
+    print(df.head())
+    print(df.info())
+df:pd.DataFrame = df
+
+target = "Price"
+x = df.drop(target, axis="columns")
+y = df[[target]]
+print(y.shape)
+print(y.head())
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2, random_state=42) # stratify=y.map(int)//1000
+
+from sklearn.tree import DecisionTreeRegressor
+
+import matplotlib.pyplot as plt
+
+# Entscheidungsbaum-Regression
+model = DecisionTreeRegressor(random_state=42, max_depth=10)
+print("fitting...")
+model.fit(x_train, y_train)
+print("predicting...")
+y_pred = model.predict(x_test)
+
+if __name__ == "__main__":
+    print(x_train.shape)
+    print(x_test.shape)
+
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor, plot_tree
+plt.figure(figsize=(12,8))
+plot_tree(model, feature_names=x.columns, filled=True, rounded=True)
+plt.title("Entscheidungsbaum")
+plt.show()
+"""
+
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse:.4f}")
+print(f"Mean Squared Error: {mse**0.5:.4f}")
+
+
+importances = model.feature_importances_
+feature_importance_df = pd.DataFrame({'Feature': x.columns, 'Importance': importances})
+
+print(feature_importance_df)
--- Exercises/Wie viel Ihr Auto Wert ist/Projekt_Woche8/data/data.py
+++ Exercises/Wie viel Ihr Auto Wert ist/Projekt_Woche8/data/data.py
@@ -3,14 +3,25 @@ from sys import path as system_paths
 from sklearn.preprocessing import OneHotEncoder
 
 # load data
-df = pd.read_csv(system_paths[0]+"/car_price_prediction.csv")
+df:pd.DataFrame = pd.read_csv(system_paths[0]+"/car_price_prediction.csv")
+
+# delete opal astra
+to_great_prices = 10_000_000
+df = df.query("Price < 160000").reset_index(drop=True)
+#df = df.query("Price > 1000").reset_index(drop=True)
+
+df = df.drop("Wheel", axis="columns")
+
+#df = df.drop("Airbags", axis="columns")
+#df["Prod. year"] = df["Prod. year"] - 2000
+
+# ToDo
 
 
 # delete unecessary features
 unecessary_features = [
     "ID",
-    "Levy",
-    "Color", # später testen, cast auf 3 featrues (rgb)
+    "Levy"
 ]
 for feature in unecessary_features:
     df = df.drop(feature, axis="columns")
@@ -18,46 +29,70 @@ for feature in unecessary_features:
 # delete important features which are not convertable
 important_features = [
     "Model",
-    #"Manufacturer"
+    "Manufacturer",
+    "Color",
+    "Category",
+    "Fuel type",
+    #"Leather interior",
+    "Doors",
+    #"Mileage"
+    #"Gear box type"
 ]
 for feature in important_features:
     df = df.drop(feature, axis="columns")
 
 # split_features
-feature = "Engine volume"
-new_feature_name = "Has turbo engine"
-df[new_feature_name] = df[feature].map(lambda txt: "Turbo" in txt)
+if False:
+    feature = "Engine volume"
+    new_feature_name = "Has turbo engine"
+    df[new_feature_name] = df[feature].map(lambda txt: "Turbo" in txt)
 
 
 # mapping the Features to numerous values
 for feature, map in [
     ("Leather interior", {"No":0, "Yes":1}),
     # soritert nach Anzahl der Türen
-    ("Doors", {"02-Mar":2, "04-May":4, ">5": 6}),
-    ("Wheel", {"Left wheel":0, "Right-hand drive":1}),
+    #("Doors", {"02-Mar":2, "04-May":4, ">5": 6}),
+    #("Wheel", {"Left wheel":0, "Right-hand drive":1}),
     ("Airbags", int),
+    #("Airbags", lambda txt: (1-int(txt) % 2)*100000), # todo believe in grade airbags
     ("Engine volume", lambda txt: float(txt.replace("Turbo",""))),
     # sortiert nach automatisierung
     ("Gear box type",{"Manual":0,"Tiptronic":1,"Variator":2,"Automatic":3}) ,
-    ("Mileage", lambda txt: int(txt[:-2]))
+    ("Mileage", lambda txt: int(txt[:-2])),
+    ("Drive wheels", lambda txt: txt == "Rear")
 ]:
     df[feature] = df[feature].map(map)
 
+#df = df.query("Mileage < 10000").reset_index(drop=True)
+
+
 
 hot_encoding_features = [
-    "Manufacturer",
-    #"Model",
-    "Category",
-    "Fuel type", # später mal testen in einem feature
-    "Drive wheels",
-    ]
+        "Manufacturer",
+        "Model",
+        "Category",
+        "Fuel type", # später mal testen in einem feature
+        #"Drive wheels",
+        "Color",
+        #"Gear box type"
+        ]
+for feature in important_features:
+    if feature in hot_encoding_features:
+        hot_encoding_features.remove(feature)
 
-encoder = OneHotEncoder(sparse_output=False)
-encoded_features = encoder.fit_transform(df[hot_encoding_features])
-encoded_df = pd.DataFrame(encoded_features, columns=encoder.get_feature_names_out(hot_encoding_features))
+if True:
+    encoder = OneHotEncoder(sparse_output=False)
+    encoded_features = encoder.fit_transform(df[hot_encoding_features])
+    encoded_df = pd.DataFrame(encoded_features, columns=encoder.get_feature_names_out(hot_encoding_features))
 
-df = df.drop(columns=hot_encoding_features) 
-df = pd.concat([df, encoded_df], axis=1) 
+    df = df.drop(columns=hot_encoding_features) 
+
+    df = pd.concat([df, encoded_df], axis=1) 
+else:
+    df = df.drop(hot_encoding_features, axis="columns")
+
+
 
 if __name__ == "__main__":
     print(df.head())
@@ -67,3 +102,15 @@ if __name__ == "__main__":
         feature = "Doors"
         for name in df[feature].unique():
             print(name)
+
+
+    for feature in df.columns.tolist():
+        print(feature)
+        import matplotlib.pyplot as plt
+        plt.figure(figsize=(10, 6))
+        plt.scatter(df[feature], df['Price'], color='blue', marker='o')
+        plt.title(feature+' vs. Price')
+        plt.xlabel(feature)
+        plt.ylabel('Price (in Euro)')
+        plt.grid(True)
+        plt.show()
