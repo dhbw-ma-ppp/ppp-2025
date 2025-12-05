77ad1aa05c055de4745278e5605d7926b826650c
Jan-Schnitzler03 <janschnitzler03@web.de>
2025-12-05 09:27:01 +0100

    Initialize main.py with necessary imports and load dataset

--- Exercises/student_performance_factors_J,M,F/main.py
+++ Exercises/student_performance_factors_J,M,F/main.py
@@ -1,2 +1,13 @@
-print('hello')
-# Hallo Felix
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.svm import SVR
+from sklearn.preprocessing import MinMaxScaler,StandardScaler, LabelEncoder
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the dataset
+data = pd.read_csv('student_performance.csv')
+
