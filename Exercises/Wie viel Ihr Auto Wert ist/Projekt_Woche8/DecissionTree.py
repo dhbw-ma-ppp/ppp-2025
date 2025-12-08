dd0e991622be751dfc93eb412b41eac9ac3d13a4
DuenenGitH <b.heinen123@icloud.com>
2025-12-08 22:17:36 +0100

    Add saving to DecissionTree

--- Exercises/Wie viel Ihr Auto Wert ist/Projekt_Woche8/DecissionTree.py
+++ Exercises/Wie viel Ihr Auto Wert ist/Projekt_Woche8/DecissionTree.py
@@ -4,12 +4,30 @@ from sklearn.metrics import mean_squared_error
 from data_selection import x, y
 from sklearn.model_selection import train_test_split
 
+import joblib
+import os 
+from sys import path as system_paths
+MODEL_FILE_PATH = os.path.join(system_paths[0], "Model_Data", 'DecissionTree.pkl')
+
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 def train_decision_tree():
     model = DecisionTreeRegressor(random_state=42, max_depth=10)
     model.fit(x_train, y_train)
     return model, x_train, x_test, y_train, y_test
+
+if __name__ == "__main__" and os.path.exists(MODEL_FILE_PATH):
+    os.remove(MODEL_FILE_PATH)
+
+if os.path.exists(MODEL_FILE_PATH):
+    print(f"Load model from: {MODEL_FILE_PATH}")
+    model = joblib.load(MODEL_FILE_PATH)
+else:
+    print("Train model with GridSearchCV...")
+    model = train_decision_tree()
+    
+    joblib.dump(model, MODEL_FILE_PATH, compress=9)
+
 
 if __name__ == "__main__":
     model, x_train, x_test, y_train, y_test = train_decision_tree()
